497d368cf04303d029718547518946ae2b22bc6e
Rachid <thiernorassididiallo22@gmail.com>
2026-01-14 20:32:44 -0500

    Mise a jour de mon portfolio

--- js/main.js
+++ js/main.js
@@ -16,44 +16,57 @@ const projects = [
     title: "Snake Game — Android / Java",
     desc: "Ce projet consiste en une application Android simple mais fonctionnelle de jeu du serpent.",
     img: "images/snake.png",
-    link: "https://github.com/tonpseudo/password-checker"
+    link: "https://github.com/Rachid224-GN/mobile"
   },
   {
     title: "Stéganographie - Appli",
     desc: "La stéganographie consiste à cacher des informations secrètes dans des supports à priori classiques afin que personne ne puisse détecter la présence de l'information cachée.",
     img: "images/steganoe.png",
-    link: "https://github.com/tonpseudo/phishdetect"
+    link: "https://github.com/Rachid224-GN/app"
   },
   {
     title: "Appli - Client/Serveur-java",
     desc: "Ce projet consiste à implémenter une communication bidirectionnelle entre un client et un serveur utilisant le langage Java.",
     img: "images/Serveur.jpeg",
-    link: "https://github.com/tonpseudo/log-inspector"
+    link: "https://github.com/Rachid224-GN/serveur"
   },
   {
     title: "Projet Commerce - POO",
     desc: "ce projet consiste à gerer les relations entre le client , l'entreprise et l'entrepôt de façon à etre capable à contrôler les produits , les composants et les commandes & en C++.",
     img: "images/Poo.png",
-    link: "https://github.com/tonpseudo/network-mapper"
+    link: "https://github.com/Rachid224-GN/POO-commerce"
   },
   {
     title: "Azure - déploiement",
     desc: "j'ai développé un site web statique qui interagit exclusivement avec des fonctions Azure pour permettre: le telechargement, l'affichage et le redimensionnement des images vers un conteneur de stockage Azure.",
     img: "images/Azure.png",
-    link: "https://github.com/tonpseudo/file-integrity-monitor"
+    link: "https://github.com/Rachid224-GN/Azure"
+  },
+  {
+    title: "Jeu du Serpent — Conception Web",
+    desc: "Jeu du serpent développé en JavaScript dans un contexte de conception web. Gestion des mouvements, collisions et du score avec une interface interactive.",
+    img: "images/jeu.png",
+    link: "https://github.com/Rachid224-GN/jeu"
+  },
+  {
+    title: "App signalisation - Architecture",
+    desc: "Développement en C d’un système de contrôle de feu de circulation sur Raspberry Pi. Projet testé et validé sur matériel réel.",
+    img: "images/feu.jpg",
+    link: "https://github.com/Rachid224-GN/Arm"
   },
   {
     title: "Appli ~ Messenger Familial",
     desc: "c'est une application de chat familial que je suis entrain de réaliser en ce moment, mais reste des concepts à mettre en place",
     img: "images/photo.jpg",
-    link: "https://github.com/tonpseudo/security-dashboard"
+    link: "https://github.com/"
   },
   {
     title: "Projet à Venir",
     desc: "En cours de realisation par Rachid.",
     img: "images/projet.jpg",
-    link: "https://github.com/tonpseudo/securechat"
+    link: "https://github.com/"
   },
+
 ];
 
 // Injection dynamique des cartes projets
